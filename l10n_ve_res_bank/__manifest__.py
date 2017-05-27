83498b1be5927e5b56251ac6eea09b780d1d9dae
Israel Lugo <hostelixisrael@gmail.com>
2017-05-26 22:25:23 -0400

    [DEL] remove files not used in module l10n_ve_res_bank

--- l10n_ve_res_bank/__manifest__.py
+++ l10n_ve_res_bank/__manifest__.py
@@ -2,12 +2,10 @@
 {
     'name': "l10n_ve_res_bank",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': "Banks for venezuela",
 
     'description': """
-        Long description of module's purpose
+        Banks for venezuela
     """,
 
     'author': "Asociacion Cooperativa BMKeros R.L",
