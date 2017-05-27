2862b2c58097581af337b2f9823506813a1ed57c
Israel Lugo <hostelixisrael@gmail.com>
2017-05-26 20:57:51 -0400

    [ADD] add module l10n_ve_res_country_state

---
+++ l10n_ve_res_country_state/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_ve_res_country_state",
+
+    'summary': "Localization states for venezuela",
+
+    'description': "Localization for venezuela res.country.state",
+
+    'author': "Asociacion Cooperativa BMKeros R.L",
+    'website': "http://www.bmkeros.org",
+    'category': 'Localization/Venezuela',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'data/res_country_state.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
