f06d913efbfe8e3c7acedd86213f94e224ca5394
Israel Lugo <hostelixisrael@gmail.com>
2017-05-26 22:42:29 -0400

    [ADD] add module l10n_ve_base

---
+++ l10n_ve_base/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_ve_base",
+
+    'summary': "Base for localization venezuela",
+
+    'description': "Base for localization venezuela",
+
+    'author': "Asociacion Cooperativa BMKeros R.L",
+    'website': "http://www.bmkeros.org",
+    'category': 'Localization/Venezuela',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'data/res_currency.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
