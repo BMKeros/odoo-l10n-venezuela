aa98128294e111d10f1abaa7788284127c20cf69
Israel Lugo <hostelixisrael@gmail.com>
2017-05-26 22:23:05 -0400

    [ADD] add module l10n_ve_res_bank

---
+++ l10n_ve_res_bank/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_ve_res_bank",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Asociacion Cooperativa BMKeros R.L",
+    'website': "http://www.bmkeros.org",
+    'category': 'Localization/Venezuela',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'data/res_bank.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
